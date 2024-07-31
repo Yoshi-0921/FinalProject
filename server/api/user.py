f726f30fad857f5d8fc120ef2b668bad9c5ee79b
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2024-07-31 22:57:11 +0900

    user db

--- server/api/user.py
+++ server/api/user.py
@@ -1,7 +1,15 @@
-from config import RESOUCE
+import sqlite3
+
+from config import RESOUCE, SQLITE_PATH
 
 
 class User(RESOUCE):
-    USER_API_PREFIX = '/users'
-    def get(self):
-        return {'users': ['Weimeng']}
+    USER_API_PREFIX = '/users/<userid>'
+    def get(self, userid):
+        conn = sqlite3.connect(SQLITE_PATH)
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM users WHERE userid == ?', [userid])
+        result = cur.fetchone()
+        cur.close()
+        conn.close()
+        return {'users': result}
