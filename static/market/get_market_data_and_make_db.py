8a8dd0e030a10770f9d4a2750d6221c95ebe96f2
Tagussan <taguchi.2718@gmail.com>
2024-07-31 22:17:08 +0900

    add market db create script and db binary

---
+++ static/market/get_market_data_and_make_db.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime
+import pandas as pd
+
+import sqlite3
+import pickle
+
+conn = sqlite3.connect('market.sqlite')
+
+# need to execute once on local environment
+def save_market_data(symbol):
+    dt = datetime(2013, 1, 1)
+    start_date = int(round(dt.timestamp()))
+    dt = datetime(2023, 1, 1)
+    end_date = int(round(dt.timestamp()))
+    df = pd.read_csv(f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={start_date}&period2={end_date}&interval=1d&events=history&includeAdjustedClose=true", parse_dates = ['Date'], index_col='Date')
+    df.to_pickle(symbol + ".pkl")
+
+def insert_into_db(symbol):
+    df = pd.read_pickle(symbol + ".pkl")
+    cur = conn.cursor()
+    for date, row in df.iterrows():
+        query = "INSERT INTO stocks(unixtimestamp, symbol, open, high, low, close, adjVolume, volume) VALUES ('{}', '{}', {}, {}, {}, {}, {}, {})".format(int(date.timestamp()), str(symbol), row['Open'], row['High'], row['Low'], row['Close'], row['Adj Close'], row['Volume'])
+        cur.execute(query)
+    conn.commit()
+
+symbols = ["NVDA", "AAPL", "MS"]
+for symbol in symbols:
+    #save_market_data(symbol)
+    insert_into_db(symbol)
+conn.close()
