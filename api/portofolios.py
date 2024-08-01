c49832a4854bee1e4e8a8e5dac146cbf653a6251
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2024-08-01 16:00:40 +0900

    add portfolio

--- api/portofolios.py
+++ api/portofolios.py
@@ -2,8 +2,18 @@ from common import AbstractResource
 from portfolio import get_portfolio_returns
 
 
-class Portfolio(AbstractResource):
-    END_POINTS = ['/portfolios/<username>']
+class PortfolioReturns(AbstractResource):
+    END_POINTS = ['/returns/<userid>']
 
-    def get(self, username):
-        return get_portfolio_returns(username)
+    def get(self, userid):
+        return get_portfolio_returns(userid)
+
+class Portfolio(AbstractResource):
+    END_POINTS = ['/portfolios/<userid>']
+
+    def get(self, userid):
+        cur = self.get_cursor()
+        cur.execute('SELECT * FROM portfolios JOIN users ON portfolios.user_id = users.id WHERE users.userid == ?', [userid])
+        result = cur.fetchall()
+        self.close_cursor()
+        return {'portfolio': result}
