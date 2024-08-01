1263d640523438c282f44f29d65f829ecec9d3b0
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2024-08-01 13:58:20 +0900

    Add ENDPOINTS

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -8,14 +8,14 @@ api = API(app)
 # -------------- USER  ---------------
 from .user import *
 
-api.add_resource(User, User.API_PREFIX)
+api.add_resource(User, *User.END_POINTS) # /users
 
 # -------------- OPENAI ---------------
 from chat import *
 
-api.add_resource(OpenAI, OpenAI.API_PREFIX)
+api.add_resource(OpenAI, *OpenAI.END_POINTS)
 
 # -------------- STOCK ---------------
 from .stock import *
 
-api.add_resource(Stock, Stock.API_PREFIX)
+api.add_resource(Stock, *Stock.END_POINTS)
