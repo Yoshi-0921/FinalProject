c7985fd3dd600d0e3e1ff578273a88615bb83f6c
JerryIshihara <hirotaka.ishihara@mail.utoronto.ca>
2024-08-02 14:14:20 +0900

    merge

--- web/tools/news.py
+++ web/tools/news.py
@@ -1,10 +1,18 @@
-import json
+import json, requests
 
 
 class NewsTools:
-    @staticmethod
-    def get_news(symbol):
-        pass
+    def _get_news(company):
+        api_key = "YOUR_NEWS_API_KEY"
+        url = f"https://newsapi.org/v2/everything?q={company}&apiKey={api_key}"
+        response = requests.get(url)
+        news_data = response.json()
+        return {company: news_data["articles"]}
+
+    # @staticmethod
+    # def get_news(companies):
+    #     news_data_list =
+    #     return json.dumps({"results": location, "temperature": "unknown"})
 
     @staticmethod
     def get_current_weather(location, unit="fahrenheit"):
