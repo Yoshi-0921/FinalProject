bcadd50b8ef5b741aa2815c8393fffbc79a5d4df
JerryIshihara <hirotaka.ishihara@mail.utoronto.ca>
2024-08-02 15:00:44 +0900

    resolve conflict

--- server/config/__init__.py
+++ server/config/__init__.py
@@ -15,14 +15,10 @@ DEBUG = os.getenv("DEBUG")
 RESOUCE = ResourceX if DEBUG else Resource
 API = ApiX if DEBUG else Api
 
-<<<<<<< HEAD
-SQLITE_PATH = "server/static/market/market.sqlite"
-=======
 
 SQLITE_FILE_NAME = "market.sqlite"
 current_script_dir = os.path.dirname(os.path.abspath(__file__))
 relative_path = os.path.join("..", "static", "market", SQLITE_FILE_NAME)
 SQLITE_PATH = os.path.abspath(os.path.join(current_script_dir, relative_path))
->>>>>>> 4d88f3c (merge)
 
 TODAY = datetime(2024, 7, 31, 9, 0, 0)
